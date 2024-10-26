bfe2b1db9de5f3cd600b16f8e19bc0df42671afb
David Amgad <davidamgad2@icloud.com>
2024-10-26 20:39:26 +0300

    feat: added the resource for the users and complete services

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,38 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { IsEmail, IsString } from 'class-validator';
+@Schema({
+  toJSON: {
+    virtuals: true,
+    versionKey: false,
+    transform: (doc, ret) => {
+      delete ret._id;
+      delete ret.__v;
+    },
+  },
+  toObject: {
+    virtuals: true,
+    versionKey: false,
+    transform: (doc, ret) => {
+      delete ret._id;
+      delete ret.__v;
+    },
+  },
+})
+export class User extends Document {
+  @Prop({ required: true })
+  @IsString()
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  @IsEmail()
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  refreshToken: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
