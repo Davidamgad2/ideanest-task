d4a333cc0fee47fcb72a2f42e1b3934774621ea0
David Amgad <davidamgad2@icloud.com>
2024-10-26 11:24:59 +0300

    build: intialized resources for users and organizations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { OrganizationsModule } from './organizations/organizations.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, OrganizationsModule],
   controllers: [],
   providers: [],
 })
