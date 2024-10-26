dd1fcd293f912374e7eaccc941816d013fab1ba0
David Amgad <davidamgad2@icloud.com>
2024-10-26 11:33:06 +0300

    feat: added config for the environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { OrganizationsModule } from './organizations/organizations.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, OrganizationsModule],
+  imports: [
+    UsersModule,
+    OrganizationsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [],
   providers: [],
 })
